9d8f714d81be2d0a2f2c807ee3311199d6c583e3
ThomasSKokholm <ThomasSKokholm@gmail.com>
2020-02-05 21:58:30 +0100

    Some extra tweaks for the tests

--- manage.py
+++ manage.py
@@ -9,32 +9,34 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 COV = coverage.coverage(
-	branch = True,
-	include='journalmylife/*',
-	omit=[
-		'journalmylife/tests/*',
-		'journalmylife/config.py',
-		'journalmylife/tests/*',
-		'journalmylife/*/__init__.py'
-	]
+    branch=True,
+    include="journal/*",
+    omit=[
+        "journal/tests/*",
+        "journal/config.py",
+        "journal/tests/*",
+        "journal/*/__init__.py",
+    ],
 )
 
 COV.start()
 
-from journalmylife import app, db, models
+from journalmylife.journal import app, db
+from journalmylife.database import models
 
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 # migrations
-manager.add_command('db', MigrateCommand)
+manager.add_command("db", MigrateCommand)
+
 
 @manager.command
 def test():
-	"""Runs the unit tests without test coverage."""
-	tests = unittest.TestLoader().discover('journalmylife/tests', pattern='test*.py')
-	result = unittest.TextTestRunner(verbosity=2).run(tests)
-	if result.wasSuccessful():
-		return 0
-	return 1
+    """Runs the unit tests without test coverage."""
+    tests = unittest.TestLoader().discover("journal/tests", pattern="test*.py")
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
