a63ca14ade48956ad8b0bec186663cea79deaf2b
Thomas S. Kokholm <ThomasSKokholm@gmail.com>
2020-02-14 13:07:40 +0100

    Added some decode_auth and test

--- journalmylife/tests/test_user_model.py
+++ journalmylife/tests/test_user_model.py
@@ -22,8 +22,22 @@ class TestUserModel(BaseTestCase):
         db.session.add(user)
         db.session.commit()
         auth_token = user.encode_auth_token(user.id)
-        print(auth_token)
         self.assertTrue(isinstance(auth_token, bytes))
+
+    def test_decode_auth_token(self):
+        user = User(
+            email='test@test.com',
+            password='test',
+            firstname='Test',
+            lastname='Tester',
+            birthday=datetime.datetime.utcnow() - datetime.timedelta(days=6570, seconds=5)
+        )
+        db.session.add(user)
+        db.session.commit()
+        auth_token = user.encode_auth_token(user.id)
+        self.assertTrue(isinstance(auth_token, bytes))
+        print(auth_token)
+        self.assertTrue(User.decode_auth_token(auth_token) == 1)
 
 if __name__ == '__main__':
     unittest.main()
