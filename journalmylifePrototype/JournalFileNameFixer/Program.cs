f8ce7c24067cfe46f4bbb1cc4b74a80fa429a2f4
ThomasSKokholm <ThomasSKokholm@gmail.com>
2021-03-11 12:58:53 +0100

    fixed unhandled IOException when allready exist

--- journalmylifePrototype/JournalFileNameFixer/Program.cs
+++ journalmylifePrototype/JournalFileNameFixer/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Text.RegularExpressions;
 
@@ -15,11 +15,22 @@ namespace JournalFileNameFixer
             string[] files = Directory.GetFiles("..");
             foreach (string i in files)
             {
+                // fjerner alle control tegn fra strengen!
                 string s = Regex.Replace(i, @"\p{C}+", string.Empty);
                 //addListItem(i);
+                // i = den orginale filnavn
                 System.Console.WriteLine(i+i.Length);
+                // s = den nye filnavn
                 System.Console.WriteLine(s+s.Length);
-
+                // if filename length != then rename file
+                //if (!File.Exists(s))
+                if((i+i.Length) != (s+s.Length)){
+                    // unhandled System.IO.IOException  'En fil, som allerede findes, kan ikke oprettes.'
+                    // if (!File.Exists(denFuldeSti))//denFuldeSti
+                    if (!File.Exists(s)){
+                        File.Move(i,s);
+                    }                    
+                }
             }
             // rename file names with U+200E
             // or rename if diffrent file/string length.
